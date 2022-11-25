4396232525e235d1f39a35698efb017b0ec11cf7
ATZCDPGAL <atzincp@gmail.com>
2022-11-25 17:05:57 -0600

    Cambios en el forms

--- ProyectoBd/Form1.Designer.cs
+++ ProyectoBd/Form1.Designer.cs
@@ -28,10 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.Insertar = new System.Windows.Forms.Button();
-            this.Actualizar = new System.Windows.Forms.Button();
-            this.Seleccionar = new System.Windows.Forms.Button();
-            this.Eliminar = new System.Windows.Forms.Button();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.panel1 = new System.Windows.Forms.Panel();
             this.label8 = new System.Windows.Forms.Label();
             this.txtId = new System.Windows.Forms.TextBox();
@@ -47,48 +44,18 @@
             this.txtArtista = new System.Windows.Forms.TextBox();
             this.txtAlbum = new System.Windows.Forms.TextBox();
             this.txtNombre = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.Eliminar = new System.Windows.Forms.Button();
+            this.Seleccionar = new System.Windows.Forms.Button();
+            this.Actualizar = new System.Windows.Forms.Button();
+            this.Insertar = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // Insertar
-            // 
-            this.Insertar.Location = new System.Drawing.Point(111, 354);
-            this.Insertar.Name = "Insertar";
-            this.Insertar.Size = new System.Drawing.Size(322, 37);
-            this.Insertar.TabIndex = 10;
-            this.Insertar.Text = "&Insertar";
-            this.Insertar.UseVisualStyleBackColor = true;
-            this.Insertar.Click += new System.EventHandler(this.Insertar_Click);
-            // 
-            // Actualizar
-            // 
-            this.Actualizar.Location = new System.Drawing.Point(111, 403);
-            this.Actualizar.Name = "Actualizar";
-            this.Actualizar.Size = new System.Drawing.Size(322, 37);
-            this.Actualizar.TabIndex = 11;
-            this.Actualizar.Text = "&Actualizar";
-            this.Actualizar.UseVisualStyleBackColor = true;
-            this.Actualizar.Click += new System.EventHandler(this.Actualizar_Click);
-            // 
-            // Seleccionar
-            // 
-            this.Seleccionar.Location = new System.Drawing.Point(111, 456);
-            this.Seleccionar.Name = "Seleccionar";
-            this.Seleccionar.Size = new System.Drawing.Size(322, 37);
-            this.Seleccionar.TabIndex = 12;
-            this.Seleccionar.Text = "&Seleccionar";
-            this.Seleccionar.UseVisualStyleBackColor = true;
-            this.Seleccionar.Click += new System.EventHandler(this.Seleccionar_Click);
-            // 
-            // Eliminar
-            // 
-            this.Eliminar.Location = new System.Drawing.Point(111, 509);
-            this.Eliminar.Name = "Eliminar";
-            this.Eliminar.Size = new System.Drawing.Size(322, 37);
-            this.Eliminar.TabIndex = 13;
-            this.Eliminar.Text = "&Eliminar";
-            this.Eliminar.UseVisualStyleBackColor = true;
-            this.Eliminar.Click += new System.EventHandler(this.Eliminar_Click);
             // 
             // panel1
             // 
@@ -106,31 +73,31 @@
             this.panel1.Controls.Add(this.txtArtista);
             this.panel1.Controls.Add(this.txtAlbum);
             this.panel1.Controls.Add(this.txtNombre);
-            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Location = new System.Drawing.Point(60, 350);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(509, 337);
+            this.panel1.Size = new System.Drawing.Size(509, 531);
             this.panel1.TabIndex = 14;
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(19, 17);
+            this.label8.Font = new System.Drawing.Font("Sans Serif Collection", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(226, 23);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(22, 20);
+            this.label8.Size = new System.Drawing.Size(33, 32);
             this.label8.TabIndex = 25;
             this.label8.Text = "Id";
             // 
             // txtId
             // 
-            this.txtId.Location = new System.Drawing.Point(130, 12);
+            this.txtId.Location = new System.Drawing.Point(74, 58);
             this.txtId.Name = "txtId";
             this.txtId.Size = new System.Drawing.Size(343, 22);
             this.txtId.TabIndex = 24;
             // 
             // txtDisquera
             // 
-            this.txtDisquera.Location = new System.Drawing.Point(130, 308);
+            this.txtDisquera.Location = new System.Drawing.Point(74, 489);
             this.txtDisquera.Name = "txtDisquera";
             this.txtDisquera.Size = new System.Drawing.Size(343, 22);
             this.txtDisquera.TabIndex = 21;
@@ -138,119 +105,208 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(14, 310);
+            this.label6.Font = new System.Drawing.Font("Sans Serif Collection", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(193, 454);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(77, 20);
+            this.label6.Size = new System.Drawing.Size(108, 32);
             this.label6.TabIndex = 20;
             this.label6.Text = "Disquera";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(14, 258);
+            this.label5.Font = new System.Drawing.Font("Sans Serif Collection", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(196, 382);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(77, 20);
+            this.label5.Size = new System.Drawing.Size(108, 32);
             this.label5.TabIndex = 19;
             this.label5.Text = "Duración";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(14, 193);
+            this.label4.Font = new System.Drawing.Font("Sans Serif Collection", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(132, 308);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(99, 40);
+            this.label4.Size = new System.Drawing.Size(240, 32);
             this.label4.TabIndex = 18;
-            this.label4.Text = "Fecha de\r\nlanzamiento";
+            this.label4.Text = "Fecha de lanzamiento";
             this.label4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             this.label4.Click += new System.EventHandler(this.label4_Click);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(19, 154);
+            this.label3.Font = new System.Drawing.Font("Sans Serif Collection", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(202, 232);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(58, 20);
+            this.label3.Size = new System.Drawing.Size(82, 32);
             this.label3.TabIndex = 17;
             this.label3.Text = "Artista";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(19, 102);
+            this.label2.Font = new System.Drawing.Font("Sans Serif Collection", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(205, 160);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(56, 20);
+            this.label2.Size = new System.Drawing.Size(79, 32);
             this.label2.TabIndex = 16;
             this.label2.Text = "Albúm";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(19, 50);
+            this.label1.Font = new System.Drawing.Font("Sans Serif Collection", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(200, 92);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(68, 20);
+            this.label1.Size = new System.Drawing.Size(95, 32);
             this.label1.TabIndex = 15;
             this.label1.Text = "Nombre";
             // 
             // txtDuracion
             // 
-            this.txtDuracion.Location = new System.Drawing.Point(130, 258);
+            this.txtDuracion.Location = new System.Drawing.Point(74, 417);
             this.txtDuracion.Name = "txtDuracion";
             this.txtDuracion.Size = new System.Drawing.Size(343, 22);
             this.txtDuracion.TabIndex = 14;
             // 
             // txtFecha
             // 
-            this.txtFecha.Location = new System.Drawing.Point(130, 193);
+            this.txtFecha.Location = new System.Drawing.Point(74, 343);
             this.txtFecha.Name = "txtFecha";
             this.txtFecha.Size = new System.Drawing.Size(343, 22);
             this.txtFecha.TabIndex = 13;
             // 
             // txtArtista
             // 
-            this.txtArtista.Location = new System.Drawing.Point(130, 154);
+            this.txtArtista.Location = new System.Drawing.Point(74, 266);
             this.txtArtista.Name = "txtArtista";
             this.txtArtista.Size = new System.Drawing.Size(343, 22);
             this.txtArtista.TabIndex = 12;
             // 
             // txtAlbum
             // 
-            this.txtAlbum.Location = new System.Drawing.Point(130, 100);
+            this.txtAlbum.Location = new System.Drawing.Point(74, 195);
             this.txtAlbum.Name = "txtAlbum";
             this.txtAlbum.Size = new System.Drawing.Size(343, 22);
             this.txtAlbum.TabIndex = 11;
             // 
             // txtNombre
             // 
-            this.txtNombre.Location = new System.Drawing.Point(130, 50);
+            this.txtNombre.Location = new System.Drawing.Point(74, 127);
             this.txtNombre.Name = "txtNombre";
             this.txtNombre.Size = new System.Drawing.Size(343, 22);
             this.txtNombre.TabIndex = 10;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft YaHei UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(89, 174);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(472, 52);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "REGISTRO DE MÚSICA";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.Color.Transparent;
+            this.label9.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.Color.DarkGray;
+            this.label9.Location = new System.Drawing.Point(179, 217);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(298, 27);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "BASE DE DATOS DE MÚSICA";
+            // 
+            // Eliminar
+            // 
+            this.Eliminar.Image = global::ProyectoBd.Properties.Resources.delete;
+            this.Eliminar.Location = new System.Drawing.Point(469, 253);
+            this.Eliminar.Name = "Eliminar";
+            this.Eliminar.Size = new System.Drawing.Size(100, 90);
+            this.Eliminar.TabIndex = 13;
+            this.Eliminar.UseVisualStyleBackColor = true;
+            this.Eliminar.Click += new System.EventHandler(this.Eliminar_Click);
+            // 
+            // Seleccionar
+            // 
+            this.Seleccionar.BackColor = System.Drawing.Color.Transparent;
+            this.Seleccionar.Image = global::ProyectoBd.Properties.Resources.check;
+            this.Seleccionar.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.Seleccionar.Location = new System.Drawing.Point(332, 253);
+            this.Seleccionar.Name = "Seleccionar";
+            this.Seleccionar.Size = new System.Drawing.Size(100, 90);
+            this.Seleccionar.TabIndex = 12;
+            this.Seleccionar.UseVisualStyleBackColor = false;
+            this.Seleccionar.Click += new System.EventHandler(this.Seleccionar_Click);
+            // 
+            // Actualizar
+            // 
+            this.Actualizar.Image = global::ProyectoBd.Properties.Resources.reload;
+            this.Actualizar.Location = new System.Drawing.Point(189, 253);
+            this.Actualizar.Name = "Actualizar";
+            this.Actualizar.Size = new System.Drawing.Size(100, 90);
+            this.Actualizar.TabIndex = 11;
+            this.Actualizar.UseVisualStyleBackColor = true;
+            this.Actualizar.Click += new System.EventHandler(this.Actualizar_Click);
+            // 
+            // Insertar
+            // 
+            this.Insertar.Image = global::ProyectoBd.Properties.Resources.more;
+            this.Insertar.Location = new System.Drawing.Point(60, 253);
+            this.Insertar.Name = "Insertar";
+            this.Insertar.Size = new System.Drawing.Size(100, 90);
+            this.Insertar.TabIndex = 10;
+            this.Insertar.UseVisualStyleBackColor = true;
+            this.Insertar.Click += new System.EventHandler(this.Insertar_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Location = new System.Drawing.Point(229, 6);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(177, 165);
+            this.panel2.TabIndex = 18;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ProyectoBd.Properties.Resources.musica;
+            this.pictureBox1.Location = new System.Drawing.Point(10, 5);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(155, 155);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 18;
+            this.pictureBox1.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(525, 549);
+            this.ClientSize = new System.Drawing.Size(636, 903);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label7);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.Eliminar);
             this.Controls.Add(this.Seleccionar);
             this.Controls.Add(this.Actualizar);
             this.Controls.Add(this.Insertar);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "Form1";
-            this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "MusicaDb";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -274,6 +330,10 @@
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
